803696a9e27c4ad915b8a40b11ae832d0a3b8dd5
MeliodasX <hamza.201001@gmail.com>
2025-06-21 01:23:52 +0500

    First commit
    - Add prettier & prettier-plugin-tailwindcss plugins for opinionated formatting.
    - Enable React Compiler (Experimental) for better performance.

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: [
+      'next',
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:prettier/recommended',
+    ],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          usePrettierrc: true,
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+    },
+  }),
+];
+
+export default eslintConfig;
